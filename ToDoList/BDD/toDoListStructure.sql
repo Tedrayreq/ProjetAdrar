2307a6d6f83cc64bfe90dcc75fdbbb5b72428868
Tedrayreq <teddu12@hotmail.fr>
2021-12-13 06:48:26 +0100

    Maj CDC + ajout ébauche ToDoList + Mise à jour SWOT + Ajout Icons, Zoning, Wireframe

---
+++ ToDoList/BDD/toDoListStructure.sql
@@ -0,0 +1,4 @@
+drop database todolist;
+create database todolist;
+create table users (id_user int primary key not null auto_increment, name_user varchar(20) not null, mail_user varchar(50) not null, tel_user varchar(20) not null;
+create table 
